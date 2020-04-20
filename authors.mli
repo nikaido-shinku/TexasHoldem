f990d39025dfa70b27e9fe60e0cab651cc1d5473
John CHen <jc2786@cornell.edu>
2020-04-19 22:30:19 -0400

    Finished prebuild

---
+++ authors.mli
@@ -0,0 +1,9 @@
+(**
+   Metadata about this submission.
+   @author  John Chen jc2786
+            Archer Luo gl355
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
